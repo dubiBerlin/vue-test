38e9b891dd55f6721ee5c28024d64d72b589e757
Dubravko <woose@web.de>
2019-07-23 08:43:03 +0200

    disable vue router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="app">
-    <router-view />
-    <!-- <MessageList :messages="messages" /> -->
+    <!-- <router-view /> -->
+    <MessageList :messages="messages" />
   </div>
 </template>
 
 <script>
 import MessageList from "./components/MessageList";
 export default {
-  data: () => ({ messages: ["Hey John", "Howdy Paco"] }),
+  data: () => ({ messages: ["Hey John", "Howdy Paco", "Bruno Wesno"] }),
 
   name: "App",
   components: {
