7e604b4ec852dccd49e22c9969ee6bc78e1be574
Dubravko <woose@web.de>
2019-07-23 11:35:10 +0200

    add a console log

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,8 +1,11 @@
 <template>
-  <li>{{ message }}</li>
+  <li class="message">{{ message }}</li>
 </template>
 <script>
 export default {
-  props: ["message"]
+  props: ["message"],
+  created() {
+    console.log("CREATED");
+  }
 };
 </script>
