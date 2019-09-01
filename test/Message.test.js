20fc1200bc61681a24e2aef492712b700bb5ad56
Dubravko <woose@web.de>
2019-09-01 20:13:31 +0200

    props validation testing

--- test/Message.test.js
+++ test/Message.test.js
@@ -13,6 +13,19 @@ describe('Message.test.js', () => {
     it('Paco is default prop', () => {
       expect(cmp.props().author).toBe('Paco')
     })
-    // it("")
+
+    describe('Validation', () => {
+      const message = createCmp({ message: 'Hello' }).vm.$options.props.message
+      it('Message is type of String', () => {
+        expect(message.type).toBe(String)
+      })
+      it('Message is required', () => {
+        expect(message.required).toBeTruthy()
+      })
+      it('Message has at least length 2', () => {
+        expect(message.validator && message.validator('a')).toBeFalsy()
+        expect(message.validator && message.validator('aa')).toBeTruthy()
+      })
+    })
   })
 })
