d9fc852e70428fb266b238ef25aa5c3923bba28d
Dubravko <woose@web.de>
2019-09-01 08:45:33 +0200

    set up Message test

---
+++ test/Message.test.js
@@ -0,0 +1,8 @@
+import { mount } from '@vue/test-utils'
+import Message from '../src/components/Message.vue'
+
+const createCmp = propsData => mount(Message, { propsData })
+
+describe('Message.test.js', () => {
+  describe('Properties', () => {})
+})
